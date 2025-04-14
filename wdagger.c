92f7333caa65d6c2b6097c0c426896c131b86615
SL4-s04 <savignanoenrico04@gmail.com>
2025-04-14 08:55:45 +0200

    Update wdagger.c

--- wdagger.c
+++ wdagger.c
@@ -9,13 +9,13 @@ char netuse[SIZE] = "net use \\\\"; //for recreate "net use" command
 char user_arg[SIZE] = " /user:";
 char pass_arg[SIZE] = " ";
     
-strcat(netuse, host);
+strcat(netuse, host); //concat the differents string
 strcat(user_arg, usr);	
 strcat(netuse, user_arg);
 strcat(pass_arg, pass);
 strcat(netuse, pass_arg);
         
-system(netuse);	
+system(netuse);	//calling the system for execute the created command
     
 }
 
@@ -39,20 +39,32 @@ FILE *f = fopen("wli.txt", "r"); //for open and reading file
 char buffer[SIZE];
 
 for (int i = 0; i < argc; i++)
-{    //with this method, we can adding more arguments after flag
-    if(strcmp(argv[i], "-u") && i + 1 < argc) { //specifies the username
-    inpf = argv[i++];   
-    }
-    if(strcmp(argv[i], "-h") && i + 1 < argc) { //specifies the hostname
-    inpf2 = argv[i++];
-    }
+{   
+    if (strcmp(argv[i], "-v") == 0) {
+            verbose = 1;
+           //with this method, we can adding more arguments after flag
+        } else if ((strcmp(argv[i], "-h") == 0) && i + 1 < argc) { 
+            inputFile = argv[++i];
+        
+	} else if ((strcmp(argv[i], "-u") == 0) && i + 1 < argc) {
+            outputFile = argv[++i];    
+            
+        } else {
+            printf("command not found: %s\n", argv[i]);
+            return 1;
+        }
 }
 
 
 if(inpf && inpf2 && buffer) {
-         system(networkUser(inpf, inpf2, buffer));
+         networkUser(inpf, inpf2, buffer);
     
-}
-
-
+} else { //if u dont add arguments, will print a message on how to use the command
+    	fprintf(stderr, "wdagger:\n");
+        fprintf(stderr, "-h: <hostname>\n");
+        fprintf(stderr, "-u: <username>");
+        fprintf(stderr, "\n\nexample: -h <hostname> -u <username>");
+        return 0;
+    }
+    return 0;
 }
